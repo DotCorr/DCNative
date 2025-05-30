5ce8a70edf3cd3ca9b52630b8364a07df8631ff0
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-05-30 17:19:51 +0200

    test update

--- packages/debug_test/dcf_go/lib/main.dart
+++ packages/debug_test/dcf_go/lib/main.dart
@@ -1,11 +1,9 @@
 
-
-import 'package:dcf_go/app/simple_app/validation_test.dart';
+import 'package:dcf_go/app/test/component+state/validation_test.dart';
 import 'package:dcflight/dcflight.dart';
 
 void main() {
-  DCFlight.start(app: ValidationTestApp
-  ());
+  DCFlight.start(app: ValidationTestApp());
 }
 
 
