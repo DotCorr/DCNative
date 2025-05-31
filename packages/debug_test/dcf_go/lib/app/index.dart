d9bcbf5b60634fdd084f602b34dd583e89d6389f
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-05-31 23:52:16 +0200

    Update index.dart

--- packages/debug_test/dcf_go/lib/app/index.dart
+++ packages/debug_test/dcf_go/lib/app/index.dart
@@ -1,13 +1,20 @@
 import 'package:dcf_go/app/test/component+state/validation_test.dart';
 import 'package:dcf_go/app/test/flatlist+state/list_state_perf.dart';
+import 'package:dcf_primitives/dcf_primitives.dart';
 import 'package:dcflight/dcflight.dart';
 
 final pagestate = Store<int>(0);
 
-class Index extends StatefulComponent{
+class Index extends StatefulComponent {
+  
   @override
   DCFComponentNode render() {
     final pagestateLocal = useStore(pagestate);
- return pagestateLocal == 0?ValidationTestApp():ListStatePerf();
+    return DCFView(
+      layout: LayoutProps(flex: 1),
+      children: [
+        pagestateLocal.state == 0 ? ValidationTestApp() : ListStatePerf()
+      ]
+    );
   }
 }
